d084dacedc82b9affefa69c9511f90e0e330449c
Ekaterina Shutova <eshutova@mirantis.com>
2016-01-14 15:03:22 +0300

    Test create/delete network from Admin tab
    Change-Id: I03a8d0dcf11539cd30f3bb64f8a2d69a4cc3bb37

--- openstack_dashboard/test/integration_tests/tests/test_networks.py
+++ openstack_dashboard/test/integration_tests/tests/test_networks.py
@@ -46,3 +46,31 @@ class TestNetworks(helpers.TestCase):
         self.assertFalse(
             networks_page.find_message_and_dismiss(messages.ERROR))
         self.assertFalse(networks_page.is_network_present(self.NETWORK_NAME))
+
+
+class TestAdminNetworks(helpers.AdminTestCase):
+    NETWORK_NAME = helpers.gen_random_resource_name("network")
+
+    def test_network_create_delete_from_admin(self):
+        """tests the network creation and deletion functionality:
+        * creates a new network through Admin panel
+        * verifies the network appears in the networks table
+        * deletes the newly created network
+        * verifies the network does not appear in the table after deletion
+        """
+        networks_page = self.home_pg.go_to_system_networkspage()
+        networks_page.create_network(name=self.NETWORK_NAME,
+                                     project=self.HOME_PROJECT)
+        self.assertTrue(
+            networks_page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(
+            networks_page.find_message_and_dismiss(messages.ERROR))
+        self.assertTrue(networks_page.is_network_present(self.NETWORK_NAME))
+        self.assertTrue(networks_page.is_network_active(self.NETWORK_NAME))
+
+        networks_page.delete_network(self.NETWORK_NAME)
+        self.assertTrue(
+            networks_page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(
+            networks_page.find_message_and_dismiss(messages.ERROR))
+        self.assertFalse(networks_page.is_network_present(self.NETWORK_NAME))
