0b6b683bc8344ad926bbed6b4613451fbcd06660
Ekaterina Shutova <eshutova@mirantis.com>
2016-01-28 19:26:02 +0300

    Added test case add subnet to the network
    Change-Id: I54f8bb2a671949595cdd4f298b63de856b422b1b
    
    Conflicts:
    	openstack_dashboard/test/integration_tests/pages/project/network/networkspage.py

--- openstack_dashboard/test/integration_tests/tests/test_networks.py
+++ openstack_dashboard/test/integration_tests/tests/test_networks.py
@@ -20,6 +20,7 @@ from openstack_dashboard.test.integration_tests.regions import messages
 class TestNetworks(helpers.TestCase):
     NETWORK_NAME = helpers.gen_random_resource_name("network")
     SUBNET_NAME = helpers.gen_random_resource_name("subnet")
+    SUBNET_NAME_2 = helpers.gen_random_resource_name("subnet")
 
     @decorators.skip_new_design
     def test_private_network_create(self):
@@ -45,6 +46,43 @@ class TestNetworks(helpers.TestCase):
             networks_page.find_message_and_dismiss(messages.SUCCESS))
         self.assertFalse(
             networks_page.find_message_and_dismiss(messages.ERROR))
+        self.assertFalse(networks_page.is_network_present(self.NETWORK_NAME))
+
+    def test_subnet_add(self):
+        """This test checks add subnet functionality
+            Steps:
+            1) Login to Horizon dashboard as demo user
+            2) Go to Project -> Network -> Networks
+            3) Create network with subnet
+            4) Add subnet for created network
+            5) Check that subnet is created (on NetworkOverview page)
+            6) Delete created subnet
+            7) Check that subnet was deleted
+            8) Check that subnet that was added during creation
+             of network is still presented
+            9) Delete created network
+        """
+        networks_page = self.home_pg.go_to_network_networkspage()
+        networks_page.create_network(self.NETWORK_NAME, self.SUBNET_NAME)
+        self.assertTrue(
+            networks_page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(
+            networks_page.find_message_and_dismiss(messages.ERROR))
+        self.assertTrue(networks_page.is_network_present(self.NETWORK_NAME))
+        self.assertTrue(networks_page.is_network_active(self.NETWORK_NAME))
+        overview = networks_page.add_subnet(self.NETWORK_NAME,
+                                            self.SUBNET_NAME_2,
+                                            network_address='10.50.0.0/16')
+        self.assertTrue(overview.is_subnet_present(self.SUBNET_NAME))
+        self.assertTrue(overview.is_subnet_present(self.SUBNET_NAME_2))
+        overview.delete_subnet(self.SUBNET_NAME_2)
+        self.assertTrue(
+            networks_page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(
+            networks_page.find_message_and_dismiss(messages.ERROR))
+        self.assertTrue(overview.is_subnet_present(self.SUBNET_NAME))
+        self.assertFalse(overview.is_subnet_present(self.SUBNET_NAME_2))
+        networks_page = overview.delete_network()
         self.assertFalse(networks_page.is_network_present(self.NETWORK_NAME))
 
 
