7318a7655fc6cd18e3d6dbeedcdc7860bf490a17
Yury Tregubov <ytregubov@mirantis.com>
2016-03-17 11:34:08 +0400

    Added test for flavor metadata update
    Implements blueprint: horizon-integration-tests-coverage
    
    Change-Id: Icb0803a2292221238a5f94118651983e57fdc217

--- openstack_dashboard/test/integration_tests/pages/admin/system/flavorspage.py
+++ openstack_dashboard/test/integration_tests/pages/admin/system/flavorspage.py
@@ -15,6 +15,11 @@ from openstack_dashboard.test.integration_tests.regions import forms
 from openstack_dashboard.test.integration_tests.regions import tables
 
 
+DEFAULT_ID = "auto"
+FLAVORS_TABLE_NAME_COLUMN = 'name'
+FLAVORS_TABLE_METADATA_COLUMN = 'extra_specs'
+
+
 class FlavorsTable(tables.TableRegion):
     name = "flavors"
 
@@ -34,10 +39,22 @@ class FlavorsTable(tables.TableRegion):
         delete_button.click()
         return forms.BaseFormRegion(self.driver, self.conf, None)
 
+    @tables.bind_row_action('update_metadata')
+    def update_metadata(self, metadata_button, row):
+        metadata_button.click()
+        return forms.MetadataFormRegion(self.driver, self.conf)
+
+    @tables.bind_row_anchor_column(FLAVORS_TABLE_METADATA_COLUMN)
+    def get_metadata_column_text(self, row_link, row):
+        return row_link.text
+
+    @tables.bind_row_anchor_column(FLAVORS_TABLE_METADATA_COLUMN)
+    def get_flavor_metadata(self, row_link, row):
+        row_link.click()
+        return forms.MetadataFormRegion(self.driver, self.conf)
+
 
 class FlavorsPage(basepage.BaseNavigationPage):
-    DEFAULT_ID = "auto"
-    FLAVORS_TABLE_NAME_COLUMN = 'name'
 
     def __init__(self, driver, conf):
         super(FlavorsPage, self).__init__(driver, conf)
@@ -48,7 +65,7 @@ class FlavorsPage(basepage.BaseNavigationPage):
         return FlavorsTable(self.driver, self.conf)
 
     def _get_flavor_row(self, name):
-        return self.flavors_table.get_row(self.FLAVORS_TABLE_NAME_COLUMN, name)
+        return self.flavors_table.get_row(FLAVORS_TABLE_NAME_COLUMN, name)
 
     def create_flavor(self, name, id_=DEFAULT_ID, vcpus=None, ram=None,
                       root_disk=None, ephemeral_disk=None, swap_disk=None):
@@ -71,3 +88,26 @@ class FlavorsPage(basepage.BaseNavigationPage):
 
     def is_flavor_present(self, name):
         return bool(self._get_flavor_row(name))
+
+    def get_metadata_column_value(self, name):
+        row = self._get_flavor_row(name)
+        return self.flavors_table.get_metadata_column_text(row)
+
+    def check_flavor_metadata(self, name, dict_with_details):
+        row = self._get_flavor_row(name)
+        metadata_form = self.flavors_table.get_flavor_metadata(row)
+        matches = []
+        actual_metadata = metadata_form.get_existing_metadata()
+        for name, value in actual_metadata.iteritems():
+            if name in dict_with_details:
+                if dict_with_details[name] in value:
+                    matches.append(True)
+        metadata_form.submit()
+        return matches
+
+    def add_custom_metadata(self, name, metadata):
+        row = self._get_flavor_row(name)
+        update_metadata_form = self.flavors_table.update_metadata(row)
+        for field_name, value in metadata.iteritems():
+            update_metadata_form.add_custom_field(field_name, value)
+        update_metadata_form.submit()
